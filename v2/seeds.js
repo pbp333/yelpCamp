6f750a3fb9b3ccb2ad017f721b4391b94944baad
Pedro Pereira <pbp333@gmail.com>
2019-02-17 19:53:49 +0000

    restyling of campgrounds detail page

--- v2/seeds.js
+++ v2/seeds.js
@@ -6,17 +6,17 @@ var data = [
 {
 	name: "Whatever1",
 	image: "http://vemco.com/wp-content/uploads/2012/09/image-banner2.jpg",
-	description: "whatever description 1"
+	description: "Aut dolorem tempore repudiandae tempora et. Et molestiae hic aut ut officiis. Assumenda perspiciatis autem nulla qui ducimus. Ullam maiores est dolorum. Ex ipsa adipisci eius aut quasi. Est sunt numquam magni voluptatem in eos."
 }, 
 {
 	name: "Whatever2",
 	image: "https://imagejournal.org/wp-content/uploads/bb-plugin/cache/23466317216_b99485ba14_o-panorama.jpg",
-	description: "whatever description 2"
+	description: "Et harum perferendis sapiente. Qui doloremque voluptatibus quis laboriosam et quaerat. Est velit iste voluptate esse laborum similique dignissimos."
 },
 {
 	name: "Whatever3",
 	image: "https://www.planwallpaper.com/static/images/sO0mRdKW.jpeg",
-	description: "whatever description 3"
+	description: "Quaerat ut qui voluptas aut accusamus nobis saepe voluptatibus. Autem qui rerum nisi nobis natus nesciunt. Fugit quam accusamus facilis eum nam natus doloremque dolores. Ut aut totam qui odit tempora nobis veniam."
 }
 ];
 
