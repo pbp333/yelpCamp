ec49e0e661175e4df08564d6556c2f4d906d696f
Pedro Pereira <pbp333@gmail.com>
2020-02-05 19:51:55 +0000

    user model refactor; added user view

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express"),
-	User = require("../models/user"),
-	passport = require("passport");
+User = require("../models/user"),
+Campground = require("../models/campground"),
+passport = require("passport");
 
 var router = express.Router();
 
@@ -16,7 +17,14 @@ router.get("/register", function(req, res) {
 });
 
 router.post("/register", function(req, res) {
-	var newUser = new User({username: req.body.username});
+	var newUser = new User({
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
+
 	User.register(newUser, req.body.password, function (err, user) {
 		if (err) {
 			console.log(err);
@@ -37,10 +45,10 @@ router.get("/login", function(req, res) {
 });
 
 router.post("/login", passport.authenticate("local", 
-	{
-		successRedirect: "/campgrounds",
-		failureRedirect: "/login"
-	}),function(req, res) {
+{
+	successRedirect: "/campgrounds",
+	failureRedirect: "/login"
+}),function(req, res) {
 });
 
 // LOGOUT
@@ -48,6 +56,29 @@ router.post("/login", passport.authenticate("local",
 router.get("/logout", function(req, res) {
 	req.logout();
 	res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+
+router.get("/users/:id", function (req, res) {
+	User.findById(req.params.id, function (err, user) {
+		if (err || !user) {
+			console.log(err);
+			req.flash("error", "User not found");
+			res.redirect("back");
+		}
+
+		Campground.find().where("author.id").equals(user._id).exec(function (err, campgrounds) {
+			if (err || !campgrounds) {
+				console.log(err);
+				req.flash("error", "User not found");
+				res.redirect("back");
+			}
+			console.log(campgrounds);
+			user.campgrounds = campgrounds;
+			res.render("users/view", { user: user });
+		});
+	});
 });
 
 module.exports = router;
